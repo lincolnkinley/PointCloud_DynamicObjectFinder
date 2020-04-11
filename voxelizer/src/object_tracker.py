102f982f95c5e41648dfd206a18be04eacb17a97
Lincoln Kinley <kinley.l@husky.neu.edu>
2020-04-11 13:14:31 -0700

    added object tracker and cleaned voxelizer

---
+++ voxelizer/src/object_tracker.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python2
+
+import rospy
+import math
+
+from scipy.optimize import linear_sum_assignment
+import numpy as np
+
+_LOST_THRESH = 40 # number of loops before an object is considered lost and will be deleted
+_DIFF_THRESH = 20 # threshold for considering an object to be a different object
+_MIN_SIZE = 1 # Minimus size, lower than this is considered noise and will be deleted
+_MAX_SIZE = 25 # Max size, higher than this means something went wrong and the object should be ignored
+
+class object_tracker:
+	def __init__(self):
+		self.dynamic_objects = []
+		self.ID_count = 0;
+		
+		
+	def update(self, blobs, time_change):
+		cost = np.empty((len(blobs), len(self.dynamic_objects), dtype=np.uint16)
+		# Cost matrix for solving best solutions. See Asignment Problem and hungarian algorithm
+		for i in values:
+			for j in i:
+				cost[i][j] = self.dynamic_objects[j].match(blobs[i])
+		row_ind, col_ind = linear_sum_assignment(values)
+		blobs_copy = np.copy(blobs)
+		# copy of the blobs and dynamic_objects. We will remove blobs and dynamic_objects from these lists as they are applied
+		for i in range(len(row_ind)):
+			if(cost[col_ind[i]][row_ind[i]] < _DIFF_THRESH):
+				# If the object is less than the _diff_thresh then we condiser it a different object
+				self.dynamic_objects[row_ind[i]].update(blobs[col_ind[i]], time_change)
+				blobs_copy[col_ind[i]] = None
+				
+		for obj in dynamic_objects:
+			if(obj.update_flag == False):
+				obj.estimate(time_change)
+				
+		# remove the Nones, remiaining blobs or objects havent been matched
+		blobs_copy[blobs_copy != np.array(None)]
+		
+		for blob in blobs_copy:
+			self.dynamic_objects.append(dynamic_object(self.ID_count, blob.size, blob.position))
+			self.ID_count += 1
+		
+		for obj in self.dynamic_objects:
+			if(obj.deletion_flag == True):
+				del(obj)
+			else
+				obj.estimate_object()
+		
+		
+
+
+		
+class dynamic_object:
+	def __init__(self, ID, size, position):
+		self.ID = ID
+		self.size = size
+		self.position = position # 1d array [x, y], relative to lidar, NOT IN VOXELS!
+		self.velocity = [0 0] # 1d array [speed (m/s), direction]
+		self.title = "unknown"
+		self.ready_flag = False # object will not be considered until true, allows time for classification
+		self.deletion_flag = False
+		self.update_flag = False # true after the object was updated, false after object is estimated. Used to keep track of which objects were seen
+		self.lost_loops = 0
+		if(self.size < _MIN_SIZE):
+			self.deletion_flag = True
+		elif(self.size > _MAX_SIZE):
+			self.deletion_flag = True
+	
+	
+	def match(self, blob)
+		score = 0.0
+		size_delta = blob.size-self.size
+		x_delta = blob.position[0] - self.position[0]
+		y_delta = blob.position[1] - self.position[1]
+		distance_delta = ((x_delta**2)+(y_delta**2))**2
+		
+		score += 0.25*distance_delta**2
+		score += size_delta**2
+		return score
+	
+	
+	def estimate_object(self):
+		# try to recognize the object based on size and velocity
+		# large objects = vehicle
+		# small slow objects = pedestrian
+		# small fast objects = cyclist
+		# very small or very large objects = noise
+		self.title = "dynamic object"
+		self.ready_flag = True # we might not need ready_flag at all...
+		# TODO
+	
+	
+	def update(self, blob, time_change):
+		# Call this every callback when the object has been detected
+		x_displacement = blob[0][0] - self.position[0]
+		y_displacement = blob[0][1] - self.position[1]
+		speed = (((x_displacement)**2+(y_displacement)**2)**0.5)/time_change
+		angle = math.atan2(y_displacement, x_displacement)
+		self.velocity = [speed, angle]
+		self.size = blob[1] # We might not need to update size, the reason we do (for now) is if the lidar detects something partially outside view it could get the initial estimate wrong which would throw off future detection
+		self.update_flag = True
+		
+		
+	def estimate(self, time_change):
+		# Call this every callback when the object is not detected
+		x = self.position[0]
+		y = self.position[1]
+		speed = self.velocity[0]
+		angle = self.velocity[1]
+		x = x + speed*math.cos(angle)*time_change
+		y = y + speed*math.sin(angle)*time_change
+		self.position = [x, y]
+		self.lost_loops += 1
+		if(self.lost_loops > _LOST_THRESH):
+			self.deletion_flag = True
--- voxelizer/src/voxelizer.py
+++ voxelizer/src/voxelizer.py
@@ -47,24 +47,16 @@ def callback(data):
 				ring = int(j[2]-_RING_MIN)
 				voxels[x_voxel][y_voxel][ring] = 255
 
-			
-	# opencv Method
-	# might be able to shorten this by integrating with the reading lidar points for loop
-	'''for scan_line in lidar_points:
-		for lidar_point in scan_line:
-			if( (abs(lidar_point[0]) < 17.5) and (abs(lidar_point[1]) < 17.5) ):
-				x_voxel = int((lidar_point[0]+17.5)*10)
-				y_voxel = int((lidar_point[1]+17.5)*10)
-				try:
-					voxels[x_voxel][y_voxel] = 255
-				except:
-					rospy.loginfo("voxel Out of range: " + str(lidar_point[0]) + " " + str(lidar_point[1]))
-	'''
 	if READY:
 		subtracted_image = cv2.subtract(voxels, PREV_IMG)
 		filtered_image = im_3d_filter(subtracted_image)
 		flat_image = flatten(filtered_image)
 		flat_image = im_2d_filter(flat_image)
+		
+		ret, thresh = cv2.threshold(flat_image, 127, 255, 0)
+		im2, contours, hierarch = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		rospy.loginfo("Objects Found = " + str(len(contours)))
+		
 		image_message = bridge.cv2_to_imgmsg(flat_image, encoding="passthrough")
 		#image_message = bridge.cv2_to_imgmsg(filtered_image, encoding="passthrough")
 		pub.publish(image_message)
@@ -97,73 +89,64 @@ def im_2d_filter(image):
 
 
 def im_3d_filter(image):
-	#rospy.loginfo(str(np.where(image != 0)))
 	raw_pixels = np.where(image == 255)
-	#rospy.loginfo(str(raw_pixels))
 	imcopy = np.zeros((_SIZE, _SIZE, _RING_MAX-_RING_MIN+1), dtype=np.uint8)
 	
 	pixels = []
+	# can speed this up by combining the two for loops
 	for i in range(len(raw_pixels[0])):
 		pixels.append([raw_pixels[0][i], raw_pixels[1][i], raw_pixels[2][i]])
 		
 	for pixel in pixels:
 		adjacent_pixels = check_nearby_pixels(pixel, image)*40
-		
-		#rospy.loginfo(str(adjacent_pixels))
 		if(adjacent_pixels > 255):
 			adjacent_pixels = 255
 		imcopy[pixel[0]][pixel[1]][pixel[2]] = int(adjacent_pixels)
-		#if(adjacent_pixels < 6):
-			#imcopy[pixel[0]][pixel[1]][pixel[2]] = 0
-	#rospy.loginfo(str(np.where(imcopy != 0)))
+
 	return imcopy
 		
 		
-		
+'''
+	
+'''
 def check_nearby_pixels(pixel_location, image):
 
 	i = 0
 	x_lower_bound = pixel_location[0] - 1
 	if(x_lower_bound < 0):
-		#rospy.loginfo("a")
 		x_lower_bound = 0
 		
 	x_upper_bound = pixel_location[0] + 2
 	if(x_upper_bound > (_SIZE)):
-		#rospy.loginfo("b")
 		x_upper_bound = _SIZE
 
 	y_lower_bound = pixel_location[1] - 1
 	if(y_lower_bound < 0):
 		y_lower_bound = 0
-		#rospy.loginfo("c")
 		
 	y_upper_bound = pixel_location[1] + 2
 	if(y_upper_bound > (_SIZE)):
 		y_upper_bound = _SIZE
-		#rospy.loginfo("d")
+		
 	if(len(pixel_location) == 3): # 3d Image
 		z_lower_bound = pixel_location[2] - 1
 		if(z_lower_bound < 0):
 			z_lower_bound = 0
-			#rospy.loginfo("e")
 		
 		z_upper_bound = pixel_location[2] + 2
 		if(z_upper_bound > ((_RING_MAX+1)-_RING_MIN)):
 			z_upper_bound = (_RING_MAX+1)-_RING_MIN
-			#rospy.loginfo("f")
 		
 		pixels = image[x_lower_bound:x_upper_bound, y_lower_bound:y_upper_bound, z_lower_bound:z_upper_bound]
 		for pixel in pixels.flatten():
 			if pixel == 255:
 				i+= 1
-	else:
+	else: # 2d image
 		pixels = image[x_lower_bound:x_upper_bound, y_lower_bound:y_upper_bound]
 		for pixel in pixels.flatten():
 			if pixel == 255:
 				i+= 1
 			
-	#rospy.loginfo(str(z_lower_bound) + " | " + str(z_upper_bound))
 	return i
 
 def main():
