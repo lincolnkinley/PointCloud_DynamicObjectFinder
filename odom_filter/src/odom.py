10641cf3ab15daa950007e17b26afe2c400b1b3d
Lincoln Kinley <kinley.l@husky.neu.edu>
2020-04-11 20:43:55 -0700

    removed old odom filter and fixed various issues in odom

--- odom_filter/src/odom.py
+++ odom_filter/src/odom.py
@@ -4,28 +4,31 @@
 import rospy
 import math
 
+from tf.transformations import euler_from_quaternion
+
 from dbw_mkz_msgs.msg import WheelSpeedReport
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Pose 
 
 class Callback:
-    def __ini__(self):
+    def __init__(self):
         self.Imu = None
         self.Wheel = None
+        self.transform_pub = rospy.Publisher('pose_info', Pose, queue_size=5)
+        self.x = 0
+        self.y = 0
 
     def callbackImu(self,msg):
         self.Imu = msg
-        self.callback()
 
     def callbackWheel(self,msg):
         self.Wheel = msg
         self.callback()
 
     def callback(self):
-        if self.Imu is not None and self.Wheel is not None:
+        if ((self.Imu != None) and (self.Wheel != None)):
             wheel_speed = []
             imu_quat = []
-            transform_pub = rospy.Publisher('pose_info', Pose, queue_size=5)
             transform_msg = Pose()
             
             #parse the wheel information into the list wheel_speed
@@ -38,7 +41,7 @@ class Callback:
             #parse the imu orientation quaternion into list imu_quat
             data_imu = str(self.Imu)
             data_split_imu = data_imu.split('\n')
-	    for ii in range(7,11):
+            for ii in range(7,11):
                 imu = data_split_imu[ii].split(':')
                 imu_quat.append(float(imu[1]))
 
@@ -46,18 +49,21 @@ class Callback:
             avg_speed = (wheel_speed[0] + wheel_speed[1] + wheel_speed[2] + wheel_speed[3])/4
         
             #time between wheel speed messages
-            time = 0.1 
+            time = 0.01 
 
             #tire is 26.7inches diameter or 0.67818m
             #d = d/2*speed * time |||| r/s * m/rad * s = m
-            total_displacement = 0.67818/2 * avg_speed * time
+            displacement = 0.67818/2 * avg_speed * time
 
             #get the angle of the displacement
-            theta = math.pi/4
+            roll, pitch, yaw = euler_from_quaternion((imu_quat[0], imu_quat[1], imu_quat[2], imu_quat[3]))
  
             #filling out the quaternion
-            transform_msg.position.x = total_displacement*math.cos(theta) 
-            transform_msg.position.y = total_displacement*math.sin(theta)
+            self.x = self.x + (displacement*math.cos(yaw))
+            self.y = self.y + (displacement*math.sin(yaw))
+
+            transform_msg.position.x = self.x
+            transform_msg.position.y = self.y
             transform_msg.position.z = 0
             transform_msg.orientation.x = imu_quat[0]
             transform_msg.orientation.y = imu_quat[1]
@@ -65,7 +71,7 @@ class Callback:
             transform_msg.orientation.w = imu_quat[3]
 
             #print transform_msg
-            transform_pub.publish(transform_msg)
+            self.transform_pub.publish(transform_msg)
 
 
 def listener():
@@ -73,8 +79,8 @@ def listener():
 
     callback = Callback()
 
-    rospy.Subscriber("/vehicle/wheel_speed_report", WheelSpeedReport, callback.callbackWheel)
-    rospy.Subscriber("/imu/imu", Imu, callback.callbackImu)
+    rospy.Subscriber("/vehicle/wheel_speed_report", WheelSpeedReport, callback.callbackWheel) # publishes about 100 times per sec
+    rospy.Subscriber("/imu/imu", Imu, callback.callbackImu) # publishes about 155 times per sec
     rospy.spin()
 
 if __name__ =='__main__':
